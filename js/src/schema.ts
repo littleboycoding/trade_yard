9037e04e621582461c1fee6c0c926f1f96e8b450
Thanawat Yodnil <littleboycoding@gmail.com>
2022-05-10 05:56:53 +0700

    Added ItemMetadata schema

--- js/src/schema.ts
+++ js/src/schema.ts
@@ -1,3 +1,5 @@
+import { PublicKey } from "@solana/web3.js";
+
 class Payload {
   instruction: number;
   args: Args;
@@ -18,7 +20,40 @@ class Args {
   }
 }
 
-const schema = new Map<any, any>([
+class ItemMetadata {
+  seller: PublicKey;
+  mint: PublicKey;
+  lamports: number;
+  payment: PublicKey;
+  item: PublicKey;
+  constructor(data: {
+    seller: Uint8Array | PublicKey;
+    mint: Uint8Array | PublicKey;
+    lamports: number;
+    payment: Uint8Array | PublicKey;
+    item: Uint8Array | PublicKey;
+  }) {
+    this.seller =
+      data.seller instanceof Uint8Array
+        ? new PublicKey(data.seller)
+        : data.seller;
+
+    this.mint =
+      data.mint instanceof Uint8Array ? new PublicKey(data.mint) : data.mint;
+
+    this.lamports = data.lamports;
+
+    this.payment =
+      data.payment instanceof Uint8Array
+        ? new PublicKey(data.payment)
+        : data.payment;
+
+    this.item =
+      data.item instanceof Uint8Array ? new PublicKey(data.item) : data.item;
+  }
+}
+
+const schema = new Map<new (args: any) => any, any>([
   [
     Payload,
     {
@@ -51,6 +86,19 @@ const schema = new Map<any, any>([
       ],
     },
   ],
+  [
+    ItemMetadata,
+    {
+      kind: "struct",
+      fields: [
+        ["seller", [32]],
+        ["mint", [32]],
+        ["lamports", "u64"],
+        ["payment", [32]],
+        ["item", [32]],
+      ],
+    },
+  ],
 ]);
 
-export { Payload, Args, schema };
+export { Payload, Args, ItemMetadata, schema };
